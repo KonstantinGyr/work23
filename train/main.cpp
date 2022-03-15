a4224c1825ae10c9e4a1165d73751ca068848881
KonstantinGyr <100041778+KonstantinGyr@users.noreply.github.com>
2022-03-15 11:48:46 +0400

    'edition3'

--- train/main.cpp
+++ train/main.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
-#include <string>
 #define MAX 20
-#define COACH "Railway carriage #  "
+#define COACH "Railway carriage # "
 #define ANSWER "How many people?"
 #define SIT_DOWN(downLoad) for (int i = 0; i < 10; ++i) { downLoad(i,train[i]);}
-#define FILLING (count)    for (int i = 0; i < 10; ++i) { count(i,train[i]);}
+#define FILLING(count) for (int i = 0; i < 10; ++i) {sum+=train[i]; count(i,train[i]);}
 
-void count(int &num,int(&train)[]){
-    int sum=0;
-    std::string full;
-    std::string empty;
-    if(train[num ] < 20){
-        full+=std::to_string(num+1);
-        full+=" ,";
+void count(int num,int quant){
+    if(quant > MAX){
+        std::cerr << COACH << num << " is full." << std::endl;
     }
     else{
-        empty+=std::to_string(num+1);
-        empty+=" ,";
-    }
-    sum+=num;
-    if(num==9) {
-        std::cout << COACH << full << " is full." << std::endl;
-        std::cout << COACH << empty << " there are places." << std::endl;
-        std::cout << "Total passengers: " << sum;
+        std::cout << COACH << num << " there are places." << std::endl;
     }
 }
-void downLoad(int &num,int (&train)[]){
-    int quant=0;
-    while (quant>MAX){
-        std::cout <<ANSWER<< std::endl<< COACH << num++ ;
-        std::cin >> quant;
-        if(quant>MAX)std::cout<<"Too much!";
-    }
-    train[num] = quant;
+void downLoad(int num,int &quant){
+    int a=0;
+    std::cout <<ANSWER<< std::endl<< COACH << num+1<<" ";
+    std::cin >> a;
+     quant=a;
 }
 int main() {
+    int sum=0;
     int train[10];
-    SIT_DOWN();
-    FILLING();
+    SIT_DOWN(downLoad);
+    FILLING(count);
+    std::cout << "Total passengers: " << sum;
     return 0;
 }
